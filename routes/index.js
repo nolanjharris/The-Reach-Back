2fb03e4dc3370c079f984e0aa92fc027d5ecdd57
Nolan James Harris <nolanjames@pop-os.localdomain>
2019-06-16 08:45:30 -0500

    fix spacing on videos page buttons

--- routes/index.js
+++ routes/index.js
@@ -2,69 +2,110 @@ var express = require("express");
 var router = express.Router();
 var passport = require("passport");
 var User = require("../models/user");
-var flas = require("connect-flash");
+var flash = require("connect-flash");
 
 
 var express = require("express");
 var router = express.Router();
 
 // homepage route
-router.get('/', function(req, res, next){
-	res.render('home');
+router.get('/', function (req, res, next) {
+  res.render('home');
 });
 
 //handle sign up logic
-router.post("/register", function(req, res, next){
-  User.findOne({username: req.body.username}, function(err, user){
-    if (err) {return res.send({'status':'err','message':err.message}); }
-    if (user) {return res.send({'status':'fail','message':'Sorry, that username is already taken'}); }
-    else {
-      var newUser = new User({username: req.body.username});
-    User.register(newUser, req.body.password, function(err, user){
-      if(err){
-        console.log(err);
-        return res.redirect(req.get("referrer"));
-      }
-      passport.authenticate("local")(req, res, function(){
-        return res.redirect(req.get("referrer"));
+router.post("/register", function (req, res, next) {
+  User.findOne({
+    username: req.body.username
+  }, function (err, user) {
+    if (err) {
+      return res.send({
+        'status': 'err',
+        'message': err.message
       });
-    });
+    }
+    if (user) {
+      return res.send({
+        'status': 'fail',
+        'message': 'Sorry, that username is already taken'
+      });
+    } else {
+      var newUser = new User({
+        username: req.body.username
+      });
+      User.register(newUser, req.body.password, function (err, user) {
+        if (err) {
+          console.log(err);
+          return res.redirect(req.get("referrer"));
+        }
+        passport.authenticate("local")(req, res, function () {
+          return res.redirect(req.get("referrer"));
+        });
+      });
     }
   });
 });
 
 //handle login logic
-router.get('/login', function(req, res, next) {
-  passport.authenticate('local', function(err, user, info) {
-    if (err) { return next(err); }
-    if (!user) { return res.redirect(req.get('referrer')); }
-    req.logIn(user, function(err) {
-      if (err) { return next(err); }
+router.get('/login', function (req, res, next) {
+  passport.authenticate('local', function (err, user, info) {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return res.redirect(req.get('referrer'));
+    }
+    req.logIn(user, function (err) {
+      if (err) {
+        return next(err);
+      }
       return res.redirect('/');
     });
   })(req, res, next);
 });
 
-router.get('/login/auth', function(req, res, next) {
-    passport.authenticate('local', function(err, user, info) {
-      if (err) {return res.send({'status':'err','message':err.message}); }
-      if (!user) {return res.send({'status':'fail','message':info.message}); }
-      req.logIn(user, function(err) {
-        if (err) { return res.send({'status':'err','message':err.message}); }
-        return res.send({'status': 'ok'});
+router.get('/login/auth', function (req, res, next) {
+    passport.authenticate('local', function (err, user, info) {
+      if (err) {
+        return res.send({
+          'status': 'err',
+          'message': err.message
+        });
+      }
+      if (!user) {
+        return res.send({
+          'status': 'fail',
+          'message': info.message
+        });
+      }
+      req.logIn(user, function (err) {
+        if (err) {
+          return res.send({
+            'status': 'err',
+            'message': err.message
+          });
+        }
+        return res.send({
+          'status': 'ok'
+        });
       });
     })(req, res, next);
   },
-  function(err, req, res, next) {
-    if (err) { return next(err); }
-    return res.send({'status':'err','message':err.message});
-});
+  function (err, req, res, next) {
+    if (err) {
+      return next(err);
+    }
+    return res.send({
+      'status': 'err',
+      'message': err.message
+    });
+  });
 
 
 //logout route
-router.get("/logout", function(req, res){
-	req.logout();
-	res.redirect("/");
+router.get("/logout", function (req, res) {
+  req.logout();
+  res.redirect("/");
 });
 
 module.exports = router;
